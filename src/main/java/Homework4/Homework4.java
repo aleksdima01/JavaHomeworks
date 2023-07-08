63c0f563c056123fd1000d721969cf5f051a90aa
Dmitrii <aleksdima@mail.ru>
2023-07-08 13:01:36 +0300

    reverse list completed

--- src/main/java/Homework4/Homework4.java
+++ src/main/java/Homework4/Homework4.java
@@ -3,15 +3,21 @@ import java.util.*;
 public class Homework4 {
     static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
+
     public static void main(String[] args) {
-task1();
+        reverseList();
     }
 
-    public static void task1() {
-        LinkedList<Integer>mylinkedList=new LinkedList<>();
+    public static void reverseList() {
+        LinkedList<Integer> myLinkedlist = new LinkedList<>();
         for (int i = 0; i < 20; i++) {
-            mylinkedList.add(random.nextInt(51));
-        }
-        System.out.println("Начальный список: " + mylinkedList+"\n");
+            myLinkedlist.add(random.nextInt(51));}
+        System.out.println(myLinkedlist);
+        Stack<Integer> stackInt = new Stack<>();
+        stackInt.addAll(myLinkedlist);
+        myLinkedlist.clear();
+        while (!stackInt.empty())
+            myLinkedlist.add(stackInt.pop());
+        System.out.println(myLinkedlist);
     }
 }
