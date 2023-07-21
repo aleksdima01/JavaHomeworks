3067f2571ec907636929d505f89a0db1fcf94766
Dmitrii <aleksdima@mail.ru>
2023-07-21 13:40:11 +0300

    OOP Homework1 completed

--- src/main/java/OOPHomework1/HotDrinkVendingMachine.java
+++ src/main/java/OOPHomework1/HotDrinkVendingMachine.java
@@ -4,22 +4,16 @@ package OOPHomework1;
 import java.util.List;
 
 public class HotDrinkVendingMachine implements VendingMachine {
+    private List<HotDrink> products;
 
-    private List<Product> products;
-    private List<HotDrink> products1;
-
-
-//        public HotDrinkVendingMachine(List<Product> products) {
-//        this.products = products;
-//    }
 
     public HotDrinkVendingMachine(List<HotDrink> products1) {
-        this.products1 = products1;
+        this.products = products1;
     }
 
 
     public HotDrink getProduct(String name) {
-        for (HotDrink product : products1) {
+        for (HotDrink product : products) {
             if (product.getName().equalsIgnoreCase(name)) {
                 return product;
             }
@@ -29,12 +23,13 @@ public class HotDrinkVendingMachine implements VendingMachine {
 
     public HotDrink getProduct(String name, Double volume, Integer temperature) {
 
-        for (HotDrink product : products1) {
+        for (HotDrink product : products) {
             if (product.getName().equalsIgnoreCase(name) && product.getVolume().equals(volume) && product.getTemp().equals(temperature))
                 return product;
         }
 
         throw new IllegalStateException(String.format("Продукт c такими параметрами name=%s:volume=%.1f:temperature=%d не найден.", name, volume, temperature));
+
     }
 
     @Override
@@ -43,14 +38,14 @@ public class HotDrinkVendingMachine implements VendingMachine {
             return false;
         }
         for (Product item : productList) {
-            products.add(item);
+            products.add((HotDrink) item);
         }
         return true;
     }
 
     @Override
     public String toString() {
-        return "Аппарат с горячими напитками:" + products1;
+        return "Аппарат с горячими напитками:" + products;
     }
 }
 
--- src/main/java/OOPHomework1/MainClass.java
+++ src/main/java/OOPHomework1/MainClass.java
@@ -7,20 +7,24 @@ public class MainClass {
     public static void main(String[] args) {
         List<Product> listOfBottles = new ArrayList<>();
         List<HotDrink> listOfHotDrinks = new ArrayList<>();
+        List<Product> listOfHotDrinks2 = new ArrayList<>();
         HotDrink hotDrink1 = new HotDrink("Кофе", 130.0, 0.3, 88);
         HotDrink hotDrink2 = new HotDrink("Чай", 80.0, 0.4, 92);
+        HotDrink hotDrink3 = new HotDrink("Кофе с молоком", 140.0, 0.3, 88);
         BottleOfWater bottleOfWater1 = new BottleOfWater("Эвиан", 80.5, 0.5);
         BottleOfWater bottleOfWater2 = new BottleOfWater("Аква Минерале", 60.0, 0.5);
         listOfBottles.add(bottleOfWater1);
         listOfBottles.add(bottleOfWater2);
         listOfHotDrinks.add(hotDrink1);
         listOfHotDrinks.add(hotDrink2);
+        listOfHotDrinks2.add(hotDrink3);
         BottleOfWaterVendingMachine machine1 = new BottleOfWaterVendingMachine(listOfBottles);
         HotDrinkVendingMachine machine2 = new HotDrinkVendingMachine(listOfHotDrinks);
         System.out.println(machine1);
+        System.out.println(machine2.addProduct(listOfHotDrinks2));
         System.out.println(machine2);
+        System.out.println(machine2.getProduct("Кофе с молоком"));
         System.out.println(machine2.getProduct("Кофе", 0.3, 88));
-        //System.out.println(machine2.getProduct("Кофе"));
-        // System.out.println(machine2.addProduct(listOfHotDrinks2);
+
     }
 }
